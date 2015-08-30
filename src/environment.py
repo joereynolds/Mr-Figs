b4663cee1f1671e86336329446d4915f46beddee
Joe Reynolds <joereynolds952@gmail.com>
2015-08-30 10:02:42 +0100

    fix email

--- src/environment.py
+++ src/environment.py
@@ -6,6 +6,7 @@ import scenes.startmenu as start_menu
 # Automatically create an array of levelobjects that are
 # linked to the next level. This enables us to just drop
 # a file into our directory and it will load perfectly
+# ..
 levels_dir = '../levels/tmx/'
 levels = os.listdir('../levels/tmx/')
 level_obj_list = [level_base.LevelBase(levels_dir + level,'NoNextScene') for level in levels]
