7e3f93a56e35f5ddec3278493faf3f436a74ab27
Joe Reynolds <joereynolds952@gmail.com>
2015-11-07 18:37:34 +0000

    remove hacky profiling

--- src/main.py
+++ src/main.py
@@ -1,9 +1,5 @@
 import pygame
 import environment
-import cProfile
-import pstats
-import io
-import sys
 
 
 class Game():
@@ -26,21 +22,12 @@ class Game():
         clock = pygame.time.Clock()
 
         while not self.done:
-            #NOTE!
-            # This exception handling here is ONLY 
-            # for profiling. It hides ALL errors which is BAD BAD BAD.
-            # The reason it hides them is so that we can profile without crashing.
-            # If you actually want to find out what's wrong with your program,
-            # for the love of god remove this exception handling!
-            try:
-                scene.process_input()
-                scene.update()
-                scene.render()
-                scene = scene.next
-                pygame.display.flip()
-                clock.tick(self.fps)
-            except:
-                self.done = True
+            scene.process_input()
+            scene.update()
+            scene.render()
+            scene = scene.next
+            pygame.display.flip()
+            clock.tick(self.fps)
         pygame.quit()
 
 
@@ -48,12 +35,5 @@ if __name__ == '__main__':
 
 
     game = Game(60)
-    pr = cProfile.Profile()
-    pr.enable()
     game.run(environment.level_obj_list[0])
-    pr.disable()
-    log = open('log.test','w')
-    sys.stdout = log #redirect all printed output to our text file. This is for profiled information only.
-    pr.print_stats()
-    log.close()
 
