eeff183880a5aa1e35dc86140de81b2d7ad95637
Joe Reynolds <joereynolds952@gmail.com>
2015-11-07 19:26:04 +0000

    refactor linking of levels

--- src/environment.py
+++ src/environment.py
@@ -13,21 +13,27 @@ def create_level_list():
         level_base.LevelBase(levels_dir + level,'NoNextScene')
         for level in levels
     ]
-    for i in range(len(level_obj_list)):
-        if i == len(level_obj_list)-1:
-            level_obj_list[i].next_level = level_obj_list[0]
-        else:
-            level_obj_list[i].next_level = level_obj_list[i+1]
+    link_levels(level_obj_list)
     return level_obj_list
+
+def link_levels(level_list):
+    """Links all of ours levels to one another.
+    i.e. when you finish a level, it knows to go 
+    to the next one
+    
+    We make sure that if we're on the final level, then we link
+    back to the first level.
+    
+    Otherwise, just link to the next level in the list"""
+    for i in range(len(level_list)):
+        if i == len(level_list)-1:
+            level_list[i].next_level = level_list[0]
+        else:
+            level_list[i].next_level = level_list[i+1]
 
 #Create our default level list and forcibly add our
 #level select menu and start menu to it.
 level_obj_list = create_level_list()
 level_obj_list.insert(0, start_menu.StartMenu())
 level_obj_list.insert(1, level_select.LevelMenu())
-for i in range(len(level_obj_list)):
-    ##If it's the last level, put the 'next_level', as the start menu
-    if i == len(level_obj_list)-1:
-        level_obj_list[i].next_level = level_obj_list[0]
-    else:
-        level_obj_list[i].next_level = level_obj_list[i+1]
+link_levels(level_obj_list)
