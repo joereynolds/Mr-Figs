1785b7172d633d36834dc430b1ff68b3a0d666cf
Joe Reynolds <joereynolds952@gmail.com>
2015-11-12 10:30:41 +0000

    linux paths

--- src/config.py
+++ src/config.py
@@ -2,8 +2,8 @@
 
 #Directories
 spritesheet_location = '../data/newtiledsheet.png'
-layout_location = 'scenes\layouts\\'
-level_location = '..\\levels\\tmx\\'
+layout_location = 'scenes/layouts/'
+level_location = '../levels/tmx/'
 
 #Magic numbers
 screen_width  = 700
