cb5f9b42085a4fbdd7950c7f1ceadef01590dbc7
joereynolds <joereynolds952@gmail.com>
2020-11-09 13:49:55 +0000

    Add WASD and HJKL controls

--- src/input_handlers/player_input_handler.py
+++ src/input_handlers/player_input_handler.py
@@ -11,7 +11,18 @@ class PlayerInputHandler():
         pygame.K_DOWN:'down',
         pygame.K_LEFT:'left',
         pygame.K_RIGHT:'right',
+
         pygame.K_SPACE:'space',
+
+        pygame.K_w: 'up',
+        pygame.K_a: 'left',
+        pygame.K_s: 'down',
+        pygame.K_d: 'right',
+
+        pygame.K_h: 'left',
+        pygame.K_j: 'down',
+        pygame.K_k: 'up',
+        pygame.K_l: 'right',
     }
 
     def __init__(self, player):
