f89bceb328dd565b8dd3caf0bd6ff58db39af35a
joereynolds <joereynolds952@gmail.com>
2020-11-13 09:14:20 +0000

    Handle collisions within the particle

--- src/bomb_particle.py
+++ src/bomb_particle.py
@@ -1,3 +1,6 @@
+import pygame
+
+import src.tile as tile
 import src.entity as entity
 import src.colours as colours
 import src.graphics as graphics
@@ -10,5 +13,21 @@ class BombParticle(entity.Entity):
         image = graphics.sprites['explosion']['sprites'][0]
         entity.Entity.__init__(self, x, y, width, height, image)
         self.minimap_colour = colours.RED_GLOW
+
+    def handle_collision(self, player, level):
+        # TODO - There's a bug in here which means that if the player's
+        # destination is outside of the particles explosion area then they
+        # won't die. This is fine in most cases but it's not accounting for
+        # solid. To test it, plant a bomb and move up into a solid wall you'll
+        # survive the blast because your destination is in the wall but really
+        # you're still on the same tile.
+        if player.destination[0] == self.rect.x and player.destination[1] == self.rect.y:
+            pygame.sprite.Sprite.kill(player)
+            return True
+        for _tile in level.tiled_level.sprites:
+            if isinstance(_tile, tile.Stateful):
+                if pygame.sprite.collide_rect(self, _tile):
+                    _tile.update()
+                    return
 
 
--- src/collision_handler.py
+++ src/collision_handler.py
@@ -23,28 +23,20 @@ class PlayerCollisionHandler(object):
     def bomb_particle_collision(self, bomb: bomb.Bomb):
         """Returns True if any of the bombs particles collide with player.
         If they do, we'll reset the level."""
-        if isinstance(bomb, type(bomb)):
-            if self.player.rect.x == bomb.rect.x and self.player.rect.y == bomb.rect.y:
-                if bomb.lifespan == 0:
-                    pygame.sprite.Sprite.kill(self.player)
-                    return True
-            if self.player.destination[0] == bomb.rect.x and self.player.destination[1] == bomb.rect.y:
-                # Bit of a hack but if 5 is our max lifespan for a bomb then it's impossible to be
-                # travelling to it and for it to have that lifespan since we would have moved
-                # and decreased the bomb's lifespan
-                if bomb.lifespan != 0 and bomb.lifespan < 5:
-                    pygame.sprite.Sprite.kill(bomb)
-                    self.player.add_bomb()
-
-        for particle in bomb.particles:
-            if self.player.destination[0] == particle.rect.x and self.player.destination[1] == particle.rect.y:
+        if self.player.rect.x == bomb.rect.x and self.player.rect.y == bomb.rect.y:
+            if bomb.lifespan == 0:
                 pygame.sprite.Sprite.kill(self.player)
                 return True
-            for _tile in self.level.tiled_level.sprites:
-                if isinstance(_tile, tile.Stateful):
-                    if pygame.sprite.collide_rect(particle, _tile):
-                        _tile.update()
-                        return
+        if self.player.destination[0] == bomb.rect.x and self.player.destination[1] == bomb.rect.y:
+            # Bit of a hack but if 5 is our max lifespan for a bomb then it's impossible to be
+            # travelling to it and for it to have that lifespan since we would have moved
+            # and decreased the bomb's lifespan
+            if bomb.lifespan != 0 and bomb.lifespan < 5:
+                pygame.sprite.Sprite.kill(bomb)
+                self.player.add_bomb()
+
+        for particle in bomb.particles:
+            particle.handle_collision(self.player, self.level)
 
     def handle_collisions(self):
         for _tile in self.level.tiled_level.sprites:
