ee772d45e5a395dc06d24f81aa70f202302e4abe
joereynolds <joereynolds952@gmail.com>
2020-11-17 08:28:54 +0000

    Add options menu

--- src/scenes/optionsmenu.py
+++ src/scenes/optionsmenu.py
@@ -5,12 +5,13 @@ import src.scenes.scenebase as scene_base
 import src.input_handlers.start_menu_input_handler as input_handler
 from src.gui.clickable import Clickable
 
-class StartMenu(scene_base.SceneBase):
+class OptionsMenu(scene_base.SceneBase):
     """Initial start menu at the start of the game"""
 
     def __init__(self):
         scene_base.SceneBase.__init__(
             self,
+            # TODO - Own input handler here
             input_handler.StartMenuInput(self)
         )
         self.components = pygame.sprite.LayeredUpdates()
@@ -19,10 +20,8 @@ class StartMenu(scene_base.SceneBase):
         center_x = size[0] // 2
 
         self.components.add([
-            Clickable(center_x, 100, 100, 50, '[S]TART GAME'),
-            Clickable(center_x, 200, 100, 50, '[L]EVEL SELECT'),
-            Clickable(center_x, 300, 100, 50, '[O]PTIONS'),
-            Clickable(center_x, 400, 100, 50, '[Q]UIT'),
+            Clickable(center_x, 100, 100, 50, '[M]USIC ON/OFF'),
+            Clickable(center_x, 200, 100, 50, '[B]ACK'),
         ])
 
     def render(self):
