5ddaf8891cbd10ec169790f482b6b52a5ef0b311
joereynolds <joereynolds952@gmail.com>
2020-11-27 16:35:39 +0000

    Make start menu work with the mouse

--- src/scenes/startmenu.py
+++ src/scenes/startmenu.py
@@ -18,16 +18,22 @@ class StartMenu(scene_base.SceneBase):
         size = pygame.display.get_window_size()
         center_x = size[0] // 2
 
-        self.components.add([
-            Clickable(center_x, 100, 100, 50, '[S]TART GAME'),
-            Clickable(center_x, 200, 100, 50, '[L]EVEL SELECT'),
-            Clickable(center_x, 300, 100, 50, '[O]PTIONS'),
-            Clickable(center_x, 400, 100, 50, '[Q]UIT'),
-        ])
+        self.menu_items = {
+            'start-button': pygame.sprite.GroupSingle(Clickable(center_x, 100, 100, 50, '[S]TART GAME')),
+            'level-select': pygame.sprite.GroupSingle(Clickable(center_x, 200, 100, 50, '[L]EVEL SELECT')),
+            'options': pygame.sprite.GroupSingle(Clickable(center_x, 300, 100, 50, '[O]PTIONS')),
+            'quit': pygame.sprite.GroupSingle(Clickable(center_x, 400, 100, 50, '[Q]UIT'))
+
+        }
 
     def render(self):
         """Fill our surface and render our buttons"""
         self.surface.fill(colours.BLUE_GLOW)
-        self.components.draw(self.surface)
-        for component in self.components:
-            component.render_text()
+        self.menu_items['start-button'].draw(self.surface)
+        self.menu_items['start-button'].sprite.render_text()
+        self.menu_items['level-select'].draw(self.surface)
+        self.menu_items['level-select'].sprite.render_text()
+        self.menu_items['options'].draw(self.surface)
+        self.menu_items['options'].sprite.render_text()
+        self.menu_items['quit'].draw(self.surface)
+        self.menu_items['quit'].sprite.render_text()
