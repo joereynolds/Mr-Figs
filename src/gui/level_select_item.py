57e4bbdf98b83e533ae3e67c52f51c750b7cc8e4
joereynolds <joereynolds952@gmail.com>
2020-11-28 12:15:01 +0000

    Get level name from map data

--- src/gui/level_select_item.py
+++ src/gui/level_select_item.py
@@ -3,12 +3,21 @@ This is the card that displays on the level select menu for
 each level.
 """
 import pygame
+import pytmx
+import src.config as config
+
 from src.gui.clickable import Clickable
 
 class LevelSelectItem(Clickable):
 
-    def __init__(self, x, y, width, height, string, level_number, image = './data/placeholder-card-image.png'):
-        Clickable.__init__(self, x, y, width, height, string)
+    def __init__(self, x, y, width, height, level_name, level_number, image = './data/placeholder-card-image.png'):
+
+        map = pytmx.TiledMap(config.level_location + level_name)
+
+        level_name = map.properties.get('display_name', 'NEED A NAME')
+
+
+        Clickable.__init__(self, x, y, width, height, level_name)
 
         self.image = pygame.image.load(image)
         self.image = pygame.transform.scale(self.image, (width, height))
