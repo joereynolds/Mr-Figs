cc827c254b2b793c5d786387b80c0ba79efbaa94
joereynolds <joereynolds952@gmail.com>
2020-11-30 17:51:30 +0000

    Correctly resize assets on minimap and start menu

--- src/resolution_asset_sizer.py
+++ src/resolution_asset_sizer.py
@@ -1,4 +1,4 @@
-
+import src.graphics as graphics
 
 class ResolutionAssetSizer():
 
@@ -12,3 +12,32 @@ class ResolutionAssetSizer():
 
 
         return font_size_map.get(screen_size, 24)
+
+    def get_button_offset(self, screen_size):
+        button_offset_map = {
+            (640, 480): 64
+        }
+        return button_offset_map.get(screen_size, 256)
+
+    def get_button_spacing(self, screen_size):
+        button_spacing_map = {
+            (640, 480): 32
+        }
+        return button_spacing_map.get(screen_size, 64)
+
+    def get_minimap_sprite_size(self, screen_size):
+        minimap_sprite_size_map = {
+            (640, 480): {
+                'sprite_placement_modifier': 0.5, 
+                'width': graphics.tile_width // 2,
+                'height': graphics.tile_height // 2,
+            }
+        }
+
+        default = {
+            'sprite_placement_modifier': 1, 
+            'width': graphics.tile_width,
+            'height': graphics.tile_height,
+        }
+
+        return minimap_sprite_size_map.get(screen_size, default)
