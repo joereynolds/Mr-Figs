1e733731dda08eb8f2b35e73f99c0d385438003f
Joe Reynolds <joereynolds952@gmail.com>
2020-12-09 08:15:24 +0000

    Remove sidebar and useless info

--- src/renderers/level_base_renderer.py
+++ src/renderers/level_base_renderer.py
@@ -6,7 +6,6 @@ import src.colours as colours
 import src.config as config
 import src.graphics as graphics
 from src.entity import Entity
-# from src.effects import Fader
 
 class LevelBaseRenderer():
 
@@ -14,7 +13,6 @@ class LevelBaseRenderer():
         self.level = level
         self.colour = colours.WHITE
         self.bomb_count = len(self.level.player.bombs)
-        # self.fader = Fader(self.level)
         print(self.level.file)
 
         width, height = pygame.display.get_window_size()
@@ -24,16 +22,7 @@ class LevelBaseRenderer():
             pygame.Rect(
                 0,
                 0,
-                width - quarter_of_screen,
-                height
-            )
-        )
-
-        self.sidebar = self.level.surface.subsurface(
-            pygame.Rect(
-                width - quarter_of_screen,
-                0,
-                quarter_of_screen,
+                width,
                 height
             )
         )
@@ -44,7 +33,7 @@ class LevelBaseRenderer():
             quarter_of_screen,
             height,
             level,
-            self.sidebar
+            self.game_area
         )
 
         self.bomb_display = DataDisplay(
@@ -52,26 +41,8 @@ class LevelBaseRenderer():
             200,
             quarter_of_screen,
             height,
-            self.sidebar,
+            self.game_area,
             'BOMBS '
-        )
-
-        self.turn_display = DataDisplay(
-            0,
-            225,
-            quarter_of_screen,
-            height,
-            self.sidebar,
-            'TURNS  TAKEN '
-        )
-
-        self.ace_turn_display = DataDisplay(
-            0,
-            250,
-            quarter_of_screen,
-            height,
-            self.sidebar,
-            'TURNS  TO  ACE  '
         )
 
         file_without_extension = os.path.splitext(self.level.file)[0]
@@ -83,7 +54,7 @@ class LevelBaseRenderer():
             0,
             quarter_of_screen,
             height,
-            self.sidebar,
+            self.game_area,
             os.path.basename(level_name)
         )
 
@@ -96,9 +67,6 @@ class LevelBaseRenderer():
         self.level.sprites.center(self.level.player.rect.center)
         self.level.sprites.draw(self.game_area)
 
-        self.sidebar.fill(colours.BLUE_BASE)
         self.minimap.render()
         self.bomb_display.render(self.level.player.remaining_bombs)
-        self.turn_display.render(self.level.player.turns_taken)
-        self.ace_turn_display.render(self.level.tiled_level.properties.get('turns_to_ace', 'TODO'))
         self.level_name_display.render()
