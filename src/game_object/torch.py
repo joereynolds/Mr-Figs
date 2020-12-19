a3bb7f9e117439aaa7cbbb7db07b1d51c6304beb
Joe Reynolds <joereynolds952@gmail.com>
2020-12-19 11:16:03 +0000

    Remove level dependency from Torch

--- src/game_object/torch.py
+++ src/game_object/torch.py
@@ -3,6 +3,5 @@ import src.environment
 import src.colours as colours
 
 class Torch(entity.Entity):
-    def __init__(self, x, y, width, height, level, image=None):
+    def __init__(self, x, y, width, height, image=None):
         entity.Entity.__init__(self, x, y, width, height, image)
-        translated = level.map_layer_for_camera.translate_rect(self.rect)
