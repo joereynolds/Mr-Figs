abc8754d0b12ec97a191195b7626f42f1765feae
Joe Reynolds <joereynolds952@gmail.com>
2021-01-01 22:09:26 +0000

    Pass 'story' through to video_tape
    We *might* want to pass text through to the tape to be displayed when
    the player collects the tape. An alternative idea is to play an audio
    file when we collect the tape. Still, I think even then we'd want
    subtitles.

--- src/game_object/video_tape.py
+++ src/game_object/video_tape.py
@@ -9,9 +9,18 @@ class VideoTape(Entity):
         It extends the entity class
         to add collision mechanics and various other bits"""
 
-    def __init__(self, x, y, width, height, image=None):
+    def __init__(
+            self, 
+            x: int, 
+            y: int, 
+            width: int, 
+            height: int, 
+            story: str, 
+            image=None
+        ):
         Entity.__init__(self, x, y, width, height, image)
 
+        self.story = story
         self.minimap_colour = colours.WHITE
 
     def handle_collision(self, tile, player, level):
--- src/tiled_map.py
+++ src/tiled_map.py
@@ -108,6 +108,7 @@ class TiledMap():
             type_map = {
                 'video_tape': {
                     **common,
+                    'story': tile_object.properties.get('story', "This story isn't written yet.")
                 },
             }
 
