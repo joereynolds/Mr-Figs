25372d60e4e87db2375f46c4b294020472b55850
Joe Reynolds <joereynolds952@gmail.com>
2021-01-01 13:25:33 +0000

    Make enemy_pathable follow arbitrary path

--- src/game_object/enemy_pathable.py
+++ src/game_object/enemy_pathable.py
@@ -3,6 +3,7 @@ An enemy that follows a pre-determined path
 """
 
 import pygame
+from pygame.math import Vector2
 import src.interpolate as interpolate
 from src.game_object.path import Path
 import src.entity as entity
@@ -26,52 +27,31 @@ class EnemyPathable(entity.Entity):
         self.destination = path.points[-1]
         self.speed =  1
         self.processed_points = []
-
-    def move_to(self, target_x, target_y):
-        """Checks to see if we've reached the destination given, if we have,
-        we can stop moving. Note that we need to use delta-time otherwise we'll get
-        schmancy interpolation effects"""
-
-        if target_x < self.rect.x:
-            self.rect.x -= self.speed
-        elif target_x > self.rect.x:
-            self.rect.x += self.speed
-        elif target_y < self.rect.y:
-            self.rect.y -= self.speed
-        elif target_y > self.rect.y:
-            self.rect.y += self.speed
-
-        if self.rect.x == target_x and self.rect.y == target_y:
-            self.processed_points.append((target_x, target_y))
-
-    def has_reached_destination(self):
-        return self.rect.x == self.destination[0] and self.rect.y == self.destination[1]
-
-    def toggle_destination(self):
-        """
-        When we arrive at our TO destination, if we get back on
-        the platform, we want to be taken to our FROM destination.
-        """
-
-        if self.destination == self.path.points[-1]:
-            self.destination = self.path.points[0]
-            return
-
-        self.destination = self.path.points[-1]
+        self.position = Vector2(x, y)
+        self.vel = Vector2(0, 0)
+        self.target = self.path.points[1]
+        self.target_radius = 25
+        self.max_speed = 2
+        self.waypoint_index = 0
 
     def handle_collision(self, tile, player, level):
+        """
+        TODO: this code is shockingly bad. Refactor when less tired and know
+        more vector math
+        """
         if pygame.sprite.collide_rect(self, player):
             pygame.sprite.Sprite.kill(player)
 
-        if player.destination[0] == self.rect.x and player.destination[1] == self.rect.y:
-            pygame.sprite.Sprite.kill(player)
+        heading = self.target - self.position
+        distance = heading.length()
 
-        if self.has_reached_destination(): 
-            self.processed_points = []
-            self.toggle_destination()
+        if heading != [0,0]:
+            heading.normalize_ip()
 
-        if not self.has_reached_destination():
-            for point in self.path.points:
-                if point not in self.processed_points:
-                    self.move_to(point[0], point[1])
+        if distance <= 2:  # We're closer than 2 pixels.
+            self.waypoint_index = (self.waypoint_index + 1) % len(self.path.points)
+            self.target = self.path.points[self.waypoint_index]
 
+        self.vel = heading * self.max_speed # Otherwise move with max_speed.
+        self.position += self.vel
+        self.rect.topleft = self.position
