bc7dd16cbbf7ad748904492b8961aa2582f9ac24
Joe Reynolds <joereynolds952@gmail.com>
2021-01-10 12:14:23 +0000

    Make fullscreen default

--- src/graphics.py
+++ src/graphics.py
@@ -32,8 +32,8 @@ def get_controller():
     return controller
 
 def get_window_surface():
-    return pygame.display.set_mode((800, 480), )
-    # return pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+    # return pygame.display.set_mode((800, 480), )
+    return pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
 def grid(x: int, y: int):
     """
