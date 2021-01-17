5b1e0c12eff9f408c6ecc6e505ae66e64b72d1d4
Joe Reynolds <joereynolds952@gmail.com>
2021-01-17 10:15:38 +0000

    Update computer terminal image

--- src/game_object/computer_terminal.py
+++ src/game_object/computer_terminal.py
@@ -1,17 +1,19 @@
 import src.entity as entity
 import src.static_scenes
 import src.colours as colours
+import src.graphics as graphics
 
 class ComputerTerminal(entity.Entity):
     def __init__(self, x, y, width, height, minigame, image=None):
         entity.Entity.__init__(self, x, y, width, height, image)
-        self.minimap_colour = colours.WHITE
         self.minigame = minigame
+        self.image = graphics.subsurf(graphics.grid(2, 10))
+        self.broken_image = graphics.subsurf(graphics.grid(1, 10))
 
     def handle_collision(self, tile, player, level):
         if player.destination[0] == self.rect.x and player.destination[1] == self.rect.y:
-            self.image.fill((255,0,0))
             level.switch_to_scene(self.minigame, minigame=True);
+            self.image = self.broken_image
 
     def handle_pre_bomb_particle_creation(self, level):
         return False
