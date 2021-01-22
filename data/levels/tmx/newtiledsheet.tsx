9ea17fa2ad5a3358cab1b3af3c0f51f4442c823b
Joe Reynolds <joereynolds952@gmail.com>
2021-01-22 17:16:52 +0000

    Remove data dir

--- data/levels/tmx/newtiledsheet.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="newtiledsheet" tilewidth="4" tileheight="4" tilecount="2880" columns="36">
- <image source="../../newtiledsheet.png" width="144" height="320"/>
-</tileset>
