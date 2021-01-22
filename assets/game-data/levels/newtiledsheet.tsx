ec13e33fde3d67c3a3fedeefc349a50746786f34
Joe Reynolds <joereynolds952@gmail.com>
2021-01-22 23:33:03 +0000

    Add scaled up old spritesheet

---
+++ assets/game-data/levels/newtiledsheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="newtiledsheet" tilewidth="32" tileheight="32" tilecount="40" columns="4">
+ <image source="../../images/newtiledsheet.png" width="144" height="320"/>
+</tileset>
