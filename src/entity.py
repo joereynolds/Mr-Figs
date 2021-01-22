8f0ccc44a75fcd049ab23d4331f128185289107d
Joe Reynolds <joereynolds952@gmail.com>
2021-01-22 23:47:49 +0000

    Allow entity to be converted to alpha

--- src/entity.py
+++ src/entity.py
@@ -4,19 +4,21 @@ import pygame
 
 class Entity(pygame.sprite.Sprite):
     """@Image = A pygame surface, probably a subsurface of a spritesheet. """
-    def __init__(self, x, y, width, height, image=None):
+    def __init__(self, x, y, width, height, image=None, alpha=False):
         pygame.sprite.Sprite.__init__(self)
-        self.image = self.load_image(image, width, height) 
+        self.image = self.load_image(image, width, height, alpha=alpha) 
         self.rect = self.image.get_rect() 
         self.rect.x = x
         self.rect.y = y
         self.solid = False
 
-    def load_image(self, image, width, height, colour=None):
+    def load_image(self, image, width, height, colour=None, alpha=False):
         """attempts to load an image, falls back onto a pygame surface otherwise"""
         if image:
             # Scale images up on the off-chance we've supplied a tile
             # bigger than the rest.
             return pygame.transform.scale(image, (int(width), int(height)))
         else:
+            if alpha:
+                return pygame.Surface((width, height)).convert_alpha()
             return pygame.Surface((width, height)).convert()
