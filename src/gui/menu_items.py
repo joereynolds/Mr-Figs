34fd17b1a79b560876c7e9175823daa96d91b64b
Joe Reynolds <joereynolds952@gmail.com>
2021-01-24 10:27:53 +0000

    Select item via mouse only if mouse has moved!

--- src/gui/menu_items.py
+++ src/gui/menu_items.py
@@ -7,15 +7,18 @@ class MenuItems():
         self.items = menu_items
         self.menu_item_map = list(self.items.keys())
         self.selected_index = selected_index
+        self.last_mouse_pos = (0,0)
 
     def get_selected_item(self):
         return self.items[self.menu_item_map[self.selected_index]].sprite
     
     def update(self):
         """Checks for mouse collisions in all our items"""
+        mouse_pos = pygame.mouse.get_pos()
         for item in self.menu_item_map:
-            if self.items[item].sprite.rect.collidepoint(pygame.mouse.get_pos()):
+            if self.last_mouse_pos != mouse_pos and self.items[item].sprite.rect.collidepoint(mouse_pos):
                 self.select_item_by_name(item)
+                self.last_mouse_pos = mouse_pos
 
     def select_item_by_name(self, name: str):
         """'selects' the sprite by name. This is useful for the mouse to
