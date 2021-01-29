6294ef6d2ed27c03741474b58e0dd1c99b49c8b8
Joe Reynolds <joereynolds952@gmail.com>
2021-01-29 16:48:01 +0000

    Slightly better bomb display

--- src/gui/bomb_display.py
+++ src/gui/bomb_display.py
@@ -3,13 +3,9 @@ Renders out a visual of how many bombs are in our inventory
 """
 import pygame
 
-from src.gui.text_element import TextElement
 from src.entity import Entity
-from src.game_object.actor import Actor
 from src.resolution_asset_sizer import ResolutionAssetSizer
 import src.graphics as graphics
-import src.colours as colours
-import src.config as config
 
 class BombDisplay(Entity):
 
@@ -20,27 +16,21 @@ class BombDisplay(Entity):
         self.width = width
         self.height = height
         self.asset_sizer = ResolutionAssetSizer()
-        self.font_size = self.asset_sizer.get_font_size(
-            pygame.display.get_window_size()
-        )
+        self.size = pygame.display.get_window_size()
+
+        self.tile_size = self.asset_sizer.get_button_size(self.size)
+        self.font_size = self.asset_sizer.get_font_size(self.size)
 
         self.surface = pygame.Surface((width, height)).convert_alpha()
         self.bomb_image = graphics.sprites['bomb']['sprites'][0],
         self.bomb_image = self.bomb_image[0]
 
-        self.bomb_image_width_scaled = round(self.bomb_image.get_width() * 4)
-        self.bomb_image_height_scaled = round(self.bomb_image.get_height() * 4)
-
         self.scaled_bomb_image = pygame.transform.scale(
             self.bomb_image,
-            (self.bomb_image_width_scaled, self.bomb_image_height_scaled)
+            (self.tile_size[0], self.tile_size[1])
         )
 
     def render(self, bomb_count):
-        """Renders text at the default position of (0,0) or otherwise
-        if a position is supplied.
-        @position = Position for the text to be rendered (optional)
-        """
         self.surface.fill((0,0,0, 0))
         for i in range(bomb_count):
             self.surface.blit(
