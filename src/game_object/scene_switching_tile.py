4ed7380ef465fba07eba6e4f734ebdb51e3418be
Joe Reynolds <joereynolds952@gmail.com>
2021-01-31 12:01:23 +0000

    Add path into scene switching tile

--- src/game_object/scene_switching_tile.py
+++ src/game_object/scene_switching_tile.py
@@ -1,12 +1,12 @@
 import src.entity as entity
-import src.static_scenes
 import src.colours as colours
+import src.config as config
 
 class SceneSwitchingTile(entity.Entity):
     def __init__(self, x, y, width, height, scene, image=None):
         entity.Entity.__init__(self, x, y, width, height, image)
         self.minimap_colour = colours.WHITE
-        self.scene = scene
+        self.scene = config.level_location + scene
 
     def handle_collision(self, tile, player, level):
         if player.destination[0] == self.rect.x and player.destination[1] + player.offset_y == self.rect.y:
